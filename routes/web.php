e0f435e6ad62cdd27c172fdbe6ef57ac3e6c1566
kkjaki <zakaria.harsoyo@gmail.com>
2025-08-23 21:04:42 +0700

    feat(routes): Add some routes for main entities

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\InternshipProgramController;
+use App\Http\Controllers\MentorController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SchoolController;
+use App\Http\Controllers\SchoolSupervisorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +19,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/schools/management', [SchoolController::class, 'management'])->name('schools.management');
+    Route::resource('schools', SchoolController::class);
+    Route::resource('schools.supervisors', SchoolSupervisorController::class)->except(['show'])->shallow();
+    Route::resource('mentors', MentorController::class)->except(['show']);
+    Route::post('mentors/{mentor}/deactivate', [MentorController::class, 'deactivate'])->name('mentors.deactivate');
+    Route::post('mentors/{mentor}/activate', [MentorController::class, 'activate'])->name('mentors.activate');
+    Route::resource('internshipPrograms', InternshipProgramController::class);
 });
 
 require __DIR__.'/auth.php';
